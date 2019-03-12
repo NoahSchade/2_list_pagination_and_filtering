4737ba7bbcc88374ae55cbbd54757fa2bc1b69e5
Noah Schade <noah9@mail.com>
2019-03-12 15:28:24 -0400

    Remove initial comments and remove useless variables.

--- js/script.js
+++ js/script.js
@@ -1,59 +1,4 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-const page = document.querySelector("body div.page");
 let people = document.querySelectorAll(".student-item");
-const div = document.createElement("DIV");
-const ul = document.createElement("UL");
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-  
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-// function showPage(num, pag) {
-//     let people = document.querySelectorAll(".student-item");
-//     let chosenNumber = num;
-//     let number = (chosenNumber * 10);
-//     let y = 0;
-
-// 	for (let i = 0; i < people.length; i++) {
-// 		    document.querySelectorAll(".student-item")[i].style.display = "none";
-// 		}
-         
-//     for (let i = number; i < (number + 10); i++) {
-//         if (i < people.length) {
-            
-//             // document.querySelectorAll(".student-item")[i].style.display = "block";
-//         }
-//     }
-// }
 
 const divForSearch = document.createElement("DIV");
 divForSearch.setAttribute("class", "student-search");
@@ -126,7 +71,6 @@ function showPage(num) {
        }
    }
 
-// const showPageVar = showPage(0);
 
 button.addEventListener("click", aSearching, false);
 document.querySelectorAll("#mySearch")[0].addEventListener("keyup", aSearching, false);
@@ -134,14 +78,9 @@ document.querySelectorAll('.pagination ul li a')[1].addEventListener("click", aS
 
 button.addEventListener("click", appendPageLinks, false);
 document.querySelectorAll("#mySearch")[0].addEventListener("keyup", appendPageLinks, false);
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
 
 function appendPageLinks() {
     let paginationLength = document.querySelectorAll(".pagination").length;
-    // document.querySelectorAll(".pagination ul")[0].parentNode.removeChild(document.querySelectorAll(".pagination ul")[0]);
     const selectUl = document.querySelectorAll(".pagination ul");
     if (selectUl.length >= 1) {document.querySelectorAll(".pagination ul")[0].parentNode.removeChild(document.querySelectorAll(".pagination ul")[0]);}
     if (paginationLength >= 1) {document.querySelectorAll(".pagination")[0].parentNode.removeChild(document.querySelectorAll(".pagination")[0])}
@@ -235,13 +174,4 @@ function aSearching() {
     }
 }
 
-// function paginationClick() {
-//     document.querySelector(".active");
-// } 
-
-
 aSearching()
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
-
-// -Write my own comments
-// -Write a search results message of no student found
